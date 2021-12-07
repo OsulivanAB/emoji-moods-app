83ffa7d04131229e8e80c32d0047c39b378b1906
Anthony <anthonybahl@mail.weber.edu>
2021-12-06 22:23:39 -0700

    Cleaned up MainSettingsFragment.java
    Signed-off-by: Anthony Bahl <anthonybahl@mail.weber.edu>

--- EmojiMoodTracker/app/src/main/java/edu/weber/cs/w01113559/emojimoodtracker/Settings/MainSettingsFragment.java
+++ EmojiMoodTracker/app/src/main/java/edu/weber/cs/w01113559/emojimoodtracker/Settings/MainSettingsFragment.java
@@ -7,46 +7,26 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.MultiSelectListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.SwitchPreferenceCompat;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-
+import edu.weber.cs.w01113559.emojimoodtracker.DashboardActivity;
 import edu.weber.cs.w01113559.emojimoodtracker.R;
-import edu.weber.cs.w01113559.emojimoodtracker.data.model.AppDatabase;
-import edu.weber.cs.w01113559.emojimoodtracker.data.model.GlobalAppDatabase;
 
 public class MainSettingsFragment extends PreferenceFragmentCompat {
-
-    private SwitchPreferenceCompat notificationSwitch;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
         setHasOptionsMenu(true);
-
-        // Setup Action Bar
-        ActionBar actionBar = ((AppCompatActivity) requireActivity()).getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setHomeButtonEnabled(true);
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setDisplayShowHomeEnabled(true);
-
-        }
+        ((DashboardActivity) requireActivity()).setHomeButton(true);
     }
 
     @Override
     public void onPrepareOptionsMenu(@NonNull Menu menu) {
         super.onPrepareOptionsMenu(menu);
+
         // Remove Settings Button
         MenuItem settingsButton = menu.findItem(R.id.action_settings);
         settingsButton.setVisible(false);
@@ -55,47 +35,19 @@ public class MainSettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // Hide Graph Button
-        FloatingActionButton graphFab = requireActivity().findViewById(R.id.graphFAB);
-        graphFab.hide();
+        setupNotificationCategory();
+    }
 
-        MultiSelectListPreference customizeEmojis = findPreference("Emojis");
-        if (customizeEmojis != null) {
-            customizeEmojis.setOnPreferenceChangeListener((preference, newValue) -> {
-
-                List<String> emojiList = new ArrayList<>();
-                emojiList.addAll( (HashSet<String>) newValue);
-                AppDatabase mDatabase = GlobalAppDatabase.getAppDatabaseInstance();
-                if (mDatabase == null) {
-                    mDatabase = GlobalAppDatabase.initializeAppDatabaseInstance();
-                }
-                mDatabase.writeEmojiList(getContext(), emojiList);
-
-                return true;
-            });
-        }
-
-        // Manage Notification Dependant Settings
-        SwitchPreferenceCompat notificationsSwitch = findPreference("notificationSwitch");
-        if (notificationsSwitch != null) {
-            // Set Dependency Visibility
-            updateNotificationDependentPreferences(notificationsSwitch.isChecked());
-            // Create click listener
-            notificationsSwitch.setOnPreferenceChangeListener((preference, newValue) -> {
-                Boolean notificationValue = (Boolean) newValue;
-                updateNotificationDependentPreferences(notificationValue);
-                if (!notificationValue) {
-                    // Notifications
-                    // ToDo: Remove any existing notifications
-                }
-                return true;
-            });
-        }
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((DashboardActivity) requireActivity()).setGraphFabVisibility(false);
     }
 
     /**
      * Updates the notification modification preferences based on whether it's parent is
      * enabled or not
+     *
      * @param notificationValue {@link Boolean} The state of the {@link SwitchPreferenceCompat} notificationSwitch.
      */
     private void updateNotificationDependentPreferences(Boolean notificationValue) {
@@ -112,4 +64,39 @@ public class MainSettingsFragment extends PreferenceFragmentCompat {
         }
     }
 
+    /**
+     * Sets up functionality of the "Notifications" category.
+     */
+    private void setupNotificationCategory() {
+
+        // Notification On/Off Switch
+        SwitchPreferenceCompat notificationsSwitch = findPreference("notificationSwitch");
+        if (notificationsSwitch != null) {
+            // Set Dependency Visibility
+            updateNotificationDependentPreferences(notificationsSwitch.isChecked());
+            // Create click listener
+            notificationsSwitch.setOnPreferenceChangeListener((preference, newValue) -> {
+                Boolean notificationSwitchState = (Boolean) newValue;
+                // Set Visibility of Dependancy Preferences
+                updateNotificationDependentPreferences((Boolean) newValue);
+                // Remove notifications if user is turning off (But not deleting them from shared preferences)
+                if (notificationSwitchState) {
+                    // Todo: Add all notifications already in the shared preferences
+                } else {
+                    // Todo: Loop through shared preferences de-activating (but not deleting) notifications.
+                }
+                return true;
+            });
+        }
+
+        // Todo: Notification Settings
+
+        // Todo: Schedule Notification
+        //  - Update Shared Preferences with notification details
+        //  - Add Notification to device
+
+        // Todo: Remove Notification
+        //  - Remove Notification from device
+        //  - Update Shared Preferences to remove notification details
+    }
 }
